ab4579d23c1378fdfbbff657591a144424353a20
ABZB <abzb13@gmail.com>
2024-06-26 20:12:25 -0400

    various functions to grab all the filepaths, and save/load config

--- dex_creator_file_loader.py
+++ dex_creator_file_loader.py
@@ -1,6 +1,59 @@
 from tkinter.filedialog import askdirectory, asksaveasfilename, askopenfilename
 from dex_creator_class import *
 
+
+def select_top_folder(dex_creation_data):
+    
+    dex_creation_data.rom_folder_path = askdirectory(title='Select Extracted ROM Folder')
+    return(dex_creation_data)
+
+def select_name_source_file(dex_creation_data, name_text_options_temp):
+    
+    dex_creation_data.pokemon_name_loading_option = name_text_options_temp
+    if(name_text_options_temp == 'Forme Insertion CSV'):
+        dex_creation_data.insertion_output_list = askopenfilename(title='Select CSV output from Forme Insertion Tool', defaultextension='.csv')
+    elif(name_text_options_temp == 'User Text File'):
+        dex_creation_data.text_file_name_list = askopenfilename(title='Select Text File where each line has that Personal File index Forme name', defaultextension='.csv')
+    else:
+        print('Default option is selected, no file to load')
+        #set to default to avoid edge cases
+        dex_creation_data.pokemon_name_loading_option = 'Default'
+    return(dex_creation_data)
+
+
+def select_game_text_file(dex_creation_data):
+    
+    dex_creation_data.game_text_path = askopenfilename(title='Select Dumped Game Text File', defaultextension='.txt')
+    return(dex_creation_data)
+
+def select_egg_source_folder(dex_creation_data, game_temp = ''):
+ 
+    if(not(game_temp in{'', 'Game'})):
+        dex_creation_data.game = game_temp
+    else:
+        print('Please select Game')
+        return
+    
+    if(dex_creation_data.game in {'SM', 'USUM'}):
+        print('This is not used for Generation VII games')
+        return
+    elif(dex_creation_data.game == 'XY'):
+        garc_path_name = 'a/2/1/3'
+    elif(dex_creation_data.game == 'ORAS'):
+        garc_path_name = 'a/1/9/0'
+    else:
+        print('Error, improper game selected: ' + str(dex_creation_data.game))
+        return
+
+    try:
+        dex_creation_data.extracted_egg_moves_folder_path = askdirectory(title='Select extracted Egg Move Folder (from GARC ' + garc_path_name + ')')
+        return(dex_creation_data)
+    except:
+        print('Was unable to load your selection, returning to main menu')
+        return(dex_creation_data)
+
+
+
 def load_dex_creator_cfg(dex_creation_data):
     
     cfg_path = askopenfilename(title='Select cfg file', defaultextension='.cfg',filetypes= [('config','.cfg')])
@@ -17,7 +70,7 @@ def load_dex_creator_cfg(dex_creation_data):
         
         if(dex_creation_data.pokemon_name_loading_option == 'Forme Insertion CSV'):
             dex_creation_data.insertion_output_list = cfg_array[4]
-        elif(dex_creation_data.pokemon_name_loading_option == 'User Text List'):
+        elif(dex_creation_data.pokemon_name_loading_option == 'User Text File'):
             dex_creation_data.text_file_name_list = cfg_array[4]
             
         if(dex_creation_data.game in {'XY', 'ORAS'}):
@@ -38,9 +91,12 @@ def load_dex_creator_cfg(dex_creation_data):
 
     return(dex_creation_data)
     
-def save_game_cfg(dex_creation_data):
+def save_cfg(dex_creation_data, game_temp = ''):
  
     cfg_path = asksaveasfilename(title='Select location to save cfg file', defaultextension='.cfg',filetypes= [('config','.cfg')])
+
+    if(game_temp != ''):
+        dex_creation_data.game = game_temp
 
     
     try:
@@ -52,7 +108,7 @@ def save_game_cfg(dex_creation_data):
             
             if(dex_creation_data.pokemon_name_loading_option == 'Forme Insertion CSV'):
                 cfg.write(dex_creation_data.insertion_output_list + '\n')
-            elif(dex_creation_data.pokemon_name_loading_option == 'User Text List'):
+            elif(dex_creation_data.pokemon_name_loading_option == 'User Text File'):
                 cfg.write(dex_creation_data.text_file_name_list + '\n')
             else:
                 cfg.write('\n')
