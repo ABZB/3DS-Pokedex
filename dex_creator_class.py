6df8006f2492388cdd852938b3de28e5ffbcc56d
ABZB <abzb13@gmail.com>
2024-06-30 01:10:38 -0400

    class functions to handle gametext filename locations

--- dex_creator_class.py
+++ dex_creator_class.py
@@ -1,43 +1,168 @@
 class Dex_creator:
-    def __init__(self):
+	def __init__(self):
         #path variables   
 
-        self.rom_folder_path = ''
-        self.game = ''
-        self.game_text_path = ''
+		self.rom_folder_path = ''
+		self.game = ''
+		self.game_text_path = ''
         
-        self.pokemon_name_loading_option = ''
+		self.pokemon_name_loading_option = ''
         
         #for loading custom Pokemon names (not present in USUM, not required in XYORAS)
         #this pulls in output from my forme insertion tool
-        self.insertion_output_list = ''
+		self.insertion_output_list = ''
         #allows use of just a custom text file if desired
-        self.text_file_name_list = ''
+		self.text_file_name_list = ''
 
         #need this only for XY/ORAS
-        self.extracted_egg_moves_folder_path = ''
+		self.extracted_egg_moves_folder_path = ''
+        
+        
+        #name array variables
+		self.pokemon_names = [] #00 terminator
+		self.forme_names = [] 
+        
+		self.type_names = []
+        
+		self.ability_names = []
+		self.ability_descriptions = []
+        
+		self.dex_data = []
+        
+		self.item_names = []
+		self.item_descriptions = []
+        
+		self.move_names = []
+		self.move_descriptions = []
+        
+		self.trainer_classes = [] #first entry is NOT empty
+		self.trainer_names = []
         
 
-        #name array variables
-        self.pokemon_names = [] #00 terminator
-        self.forme_names = [] 
-        
-        self.type_names = []
-        
-        self.ability_names = []
-        self.ability_descriptions = []
-        
-        self.dex_data = []
-        
-        self.item_names = []
-        self.item_descriptions = []
-        
-        self.move_names = []
-        self.move_descriptions = []
-        
-        self.trainer_classes = [] #first entry is NOT empty
-        self.trainer_names = []
-        
+		#text file locations
+	def gametext_file_number(self, target):
+		match target:
+			case 'ability_descriptions':			
+				match self.game:		
+					case 'XY':	
+						return(33)
+					case 'ORAS':	
+						return(36)
+					case 'SM':	
+						return(97)
+					case 'USUM':	
+						return(102)
+			case 'ability_names':			
+				match self.game:		
+					case 'XY':	
+						return(34)
+					case 'ORAS':	
+						return(37)
+					case 'SM':	
+						return(96)
+					case 'USUM':	
+						return(101)
+			case 'dex_data':			
+				match self.game:		
+					case 'XY':	
+						return(6)
+					case 'ORAS':	
+						return(6)
+					case 'SM':	
+						return(119)
+					case 'USUM':	
+						return(124)
+			case 'forme_names':			
+				match self.game:		
+					case 'XY':	
+						return(5)
+					case 'ORAS':	
+						return(5)
+					case 'SM':	
+						return(0)
+					case 'USUM':	
+						return(0)
+			case 'item_descriptions':			
+				match self.game:		
+					case 'XY':	
+						return(99)
+					case 'ORAS':	
+						return(117)
+					case 'SM':	
+						return(35)
+					case 'USUM':	
+						return(39)
+			case 'item_names':			
+				match self.game:		
+					case 'XY':	
+						return(98)
+					case 'ORAS':	
+						return(116)
+					case 'SM':	
+						return(36)
+					case 'USUM':	
+						return(40)
+			case 'move_descriptions':			
+				match self.game:		
+					case 'XY':	
+						return(15)
+					case 'ORAS':	
+						return(16)
+					case 'SM':	
+						return(112)
+					case 'USUM':	
+						return(117)
+			case 'move_names':			
+				match self.game:		
+					case 'XY':	
+						return(14)
+					case 'ORAS':	
+						return(14)
+					case 'SM':	
+						return(113)
+					case 'USUM':	
+						return(118)
+			case 'pokemon_names':			
+				match self.game:		
+					case 'XY':	
+						return(80)
+					case 'ORAS':	
+						return(98)
+					case 'SM':	
+						return(55)
+					case 'USUM':	
+						return(60)
+			case 'trainer_classes':			
+				match self.game:		
+					case 'XY':	
+						return(19)
+					case 'ORAS':	
+						return(21)
+					case 'SM':	
+						return(106)
+					case 'USUM':	
+						return(111)
+			case 'trainer_names':			
+				match self.game:		
+					case 'XY':	
+						return(20)
+					case 'ORAS':	
+						return(22)
+					case 'SM':	
+						return(105)
+					case 'USUM':	
+						return(110)
+			case 'type_names':			
+				match self.game:		
+					case 'XY':	
+						return(17)
+					case 'ORAS':	
+						return(18)
+					case 'SM':	
+						return(107)
+					case 'USUM':	
+						return(112)
+
 
 
 class GARC_file_info:
